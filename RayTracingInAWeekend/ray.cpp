12d8093720c8ef352f6485121bdd6638ea466586
Abhijit Bhelande <abhelande@gmail.com>
2019-07-02 23:33:27 -0700

    Delete these

--- RayTracingInAWeekend/ray.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ray.cpp
-//  RayTracingInAWeekend
-//
-//  Created by Abhijit Bhelande on 8/19/18.
-//  Copyright © 2018 Abhijit Bhelande. All rights reserved.
-//
-
-#include "ray.hpp"
--- RayTracingInAWeekend/vec3.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  vec3.cpp
-//  RayTracingInAWeekend
-//
-//  Created by Abhijit Bhelande on 8/19/18.
-//  Copyright © 2018 Abhijit Bhelande. All rights reserved.
-//
-
-#include "vec3.hpp"
